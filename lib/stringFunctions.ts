127be80692b426a14d90a66874aaa77d61db3789
timclaason <tim.claason@mastery.net>
2023-01-26 16:20:35 -0600

    Postal Code Functions

--- lib/stringFunctions.ts
+++ lib/stringFunctions.ts
@@ -1,3 +1,18 @@
 export const removeDiacritics = (input: string | undefined): string => {
   return input ?? ''.normalize('NFKD').replace(/[\u0300-\u036f]/g, '')
 }
+
+export const matchesRegex = (input: string, regexp: RegExp): boolean => {
+  return regexp.test(input)
+}
+
+export const isCanadianPostalCode = (input: string): boolean => {
+  return matchesRegex(
+    input,
+    new RegExp(/([ABCEGHJKLMNPRSTVXY]\d)([ABCEGHJKLMNPRSTVWXYZ]\d){2}/i)
+  )
+}
+
+export const isUSPostalCode = (input: string): boolean => {
+  return matchesRegex(input, new RegExp('^\\d{5}(-{0,1}\\d{4})?$'))
+}
